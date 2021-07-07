547059801d9ad092eeb23ca9b8be09a2638fe9d3
yaochangjian <changjian@89trillion.com>
2021-07-07 19:42:14 +0800

    second commit

--- internal/service/GiftCodeService_test.go
+++ internal/service/GiftCodeService_test.go
@@ -2,34 +2,38 @@ package service
 
 import (
 	"RedisGiftCode/StructInfo"
+	"RedisGiftCode/internal/utils"
 	"testing"
 )
 
 func TestCreateGiftCodeService(t *testing.T) {
+	utils.InitClient()
 	giftContent := StructInfo.GiftContentList{
-		GoldCoins:111,
-		Diamonds:222,
-		Props:333,
-		Heroes:444,
-		Creeps:555,
+		GoldCoins: 111,
+		Diamonds:  222,
+		Props:     333,
+		Heroes:    444,
+		Creeps:    555,
 	}
 	GiftCodeInfo := StructInfo.GiftCodeInfo{
-		GiftDes:"desc",
-		AvailableTimes:100000,
-		ValidPeriod:4,
-		User: "tom",
-		ContentList:giftContent,
+		GiftDes:        "desc",
+		AvailableTimes: 100000,
+		ValidPeriod:    4,
+		User:           "tom",
+		ContentList:    giftContent,
 	}
 	code := CreateGiftCodeService(GiftCodeInfo)
 	t.Log(code)
 }
 
 func TestGetGiftCodeInfoService(t *testing.T) {
-	GiftInfo := GetGiftCodeInfoService("JI310XOC")
+	utils.InitClient()
+	GiftInfo := GetGiftCodeInfoService("A4UJTDLV")
 	t.Log(GiftInfo)
 }
 
 func TestVerifyFiftCodeService(t *testing.T) {
-	ContentInfo := VerifyFiftCodeService("JI310XOC","tom")
+	utils.InitClient()
+	ContentInfo := VerifyFiftCodeService("A4UJTDLV", "tom")
 	t.Log(ContentInfo)
 }
